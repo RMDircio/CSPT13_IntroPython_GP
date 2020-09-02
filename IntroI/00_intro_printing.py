465a77d041dfea45361aadb93129f1df37cb81b1
Regina Dircio <reginadircio@gmail.com>
2020-09-01 21:44:41 -0500

    Lecture Notes

--- IntroI/00_intro_printing.py
+++ IntroI/00_intro_printing.py
@@ -18,15 +18,16 @@ first_name = "Tom"
 # num = 23.87
 
 # # f strings
+# print(f"this is a number: ({5+5})") # I can add non numbers here too
 # my_string = "    this is a string tom    "
 # print(my_string)
 
-# print(my_string.strip())
+# # print(my_string.strip()) # takes away whitespaces on both sides
 # print(len(my_string))
-# print(len(my_string.strip()))
+# print(len(my_string.strip())) # loses 8 character
 
 
-# print(f"Hello CSPT13 and           {len('this is a test')}     {first_name}.......".strip())
+print(f"Hello CSPT13 and           {len('this is a test')}     {first_name}.......".strip())
 # print("something on a new line")
 
 # first_name
