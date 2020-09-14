049920eba377cf9cb9f819e7f78f73efff12db69
Regina Dircio <reginadircio@gmail.com>
2020-09-13 23:14:27 -0500

    Cleaned up

--- IntroIII/intro/11_args.py
+++ IntroIII/intro/11_args.py
@@ -9,6 +9,7 @@ def f1(a, b):
 
 print(f1(1, 2))
 
+# ----------------------------------------------------------------
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
@@ -28,8 +29,10 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
+# add an *
 print(f2(*a))    # Should print 22
 
+# -----------------------------------------------------------------
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
@@ -41,6 +44,7 @@ print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
 
+# -----------------------------------------------------------------
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
 #
@@ -50,8 +54,8 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 def f4(**kwargs):
-  # for k, v in kwargs.items():
-  #   print(f'key: {k}, value: {v}')
+  # for key, value in kwargs.items():
+  #   print(f'key: {key}, value: {value}')
 
   # Alternate:
   for k in kwargs:
